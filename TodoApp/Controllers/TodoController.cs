ea6d30bb6b1585c568cf96651d4e3d29deec542a
Sully <jackswilam@gmail.com>
2022-04-01 04:04:14 +0200

    Build simple restful crud operations

--- TodoApp/Controllers/TodoController.cs
+++ TodoApp/Controllers/TodoController.cs
@@ -1,4 +1,9 @@
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Data;
+using TodoApp.Models;
 
 namespace TodoApp.Controllers
 {
@@ -6,10 +11,80 @@ namespace TodoApp.Controllers
     [ApiController]
     public class TodoController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult TestRun()
+        private readonly ApiDbContext _context;
+
+        public TodoController(ApiDbContext context)
         {
-            return Ok("success");
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetItemsAsync()
+        {
+            var items = await _context.Items.ToListAsync();
+
+            return Ok(items);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetItemAsync(int id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(i => i.Id == id);
+
+            if (item is null)
+                return NotFound();
+
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateItemAsync(Item item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new JsonResult("Something went wrong!") { StatusCode = 500 };
+            }
+
+            await _context.Items.AddAsync(item);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetItem", new { item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateItemAsync(int id, Item item)
+        {
+            if (id != item.Id)
+                return BadRequest();
+
+            var existItem = await _context.Items.FirstOrDefaultAsync(i => i.Id == id);
+
+            if (existItem is null)
+                return NotFound();
+
+            existItem.Title = item.Title;
+            existItem.Description = item.Description;
+            existItem.Done = item.Done;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItemAsync(int id)
+        {
+            var existItem = await _context.Items.FirstOrDefaultAsync(i => i.Id == id);
+
+            if (existItem is null)
+                return NotFound();
+
+            _context.Remove(existItem);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(existItem);
         }
     }
 }
--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
 
 Packages:
+	
 	EFCore Sqlite: dotnet add package Microsoft.EntityFrameworkCore.Sqlite
+	
 	EFCore Tools: dotnet add package Microsoft.EntityFrameworkCore.Tools 
